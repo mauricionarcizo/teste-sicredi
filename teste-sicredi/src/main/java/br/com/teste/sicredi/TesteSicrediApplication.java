5d4fe72b1c1a1cf331dedd29946c013021de9938
Mauricio Schwartzhaupt Narcizo <mauricio@oc1531085117.ibm.com>
2021-07-14 08:44:39 -0300

    commit inicial

---
+++ teste-sicredi/src/main/java/br/com/teste/sicredi/TesteSicrediApplication.java
@@ -0,0 +1,13 @@
+package br.com.teste.sicredi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteSicrediApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteSicrediApplication.class, args);
+	}
+
+}
