0b84ce377fbb6c6c951df3b337e59fdc76d0e693
root <root@oc1531085117.ibm.com>
2021-07-20 09:20:06 -0300

    Alterado mapeamento de packages do swagger

--- src/main/java/br/com/teste/sicredi/configs/SpringFoxConfiguration.java
+++ src/main/java/br/com/teste/sicredi/configs/SpringFoxConfiguration.java
@@ -6,22 +6,25 @@ import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
+import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-@Import(SpringDataRestConfiguration.class)
+@Import({BeanValidatorPluginsConfiguration.class})
 public class SpringFoxConfiguration extends WebMvcConfigurationSupport {
 
 	@Bean
 	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
-				.paths(PathSelectors.any()).build();
+		return new Docket(DocumentationType.SWAGGER_2)          
+			      .select()                                       
+			      .apis(RequestHandlerSelectors.basePackage("br.com.teste.sicredi.controllers"))
+			      .paths(PathSelectors.any())                     
+			      .build();
 	}
 
 	@Override
